df4d068bb4e20d67b200552783830ad2f360a0f4
Andres Ruiz Graciano <aandres.r98@gmail.com>
2018-11-17 00:56:23 -0500

    added new view with car information

--- app/src/main/java/com/esteban/rentcar/CarDetail.kt
+++ app/src/main/java/com/esteban/rentcar/CarDetail.kt
@@ -1,15 +1,66 @@
 package com.esteban.rentcar
 
+import android.app.ProgressDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.esteban.rentcar.services.IRentyApi
 import com.squareup.picasso.Picasso
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_car_detail.*
 
 class CarDetail : AppCompatActivity() {
+    var disposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_car_detail)
+        var progressDialog = ProgressDialog(this)
+        progressDialog.setMessage("Retraiving data")
+        progressDialog.setCancelable(false)
+        progressDialog.show()
+
+        val rentyServe by lazy {
+            IRentyApi.create("https://renty-heroku.herokuapp.com")
+        }
+
+        disposable = rentyServe.getCarDetails(4).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        {
+                            response ->
+                            for (car in response) {
+                                id_car.text = car.id.toString()
+                                type.text = car.type
+                                brand.text = car.brand
+                                model.text = car.model
+                                price.text = car.price.toString()
+                                rental_id.text = car.rental.id.toString()
+                                rental_id.text = car.rental.name
+                                //thumbnail.layoutParams.height = 200
+                                //thumbnail.layoutParams.width = 200
+                                Picasso.get().load(car.thumbnail).into(thumbnail)
+                                plate.text = car.plate
+                                rating.text = car.rating.toString()
+                                capacity.text = car.capacity.toString()
+                                transmission.text = car.transmission
+                                doors.text = car.doors.toString()
+                                color.text = car.color
+                                kms.text = car.kms.toString()
+
+                                for(url in car.picture) {
+                                    Picasso.get().load(url).into(images)
+                                }
+                            }
+                            progressDialog.dismiss()
+                        },
+                        {
+                            error ->
+                            Log.e("errrrrrrr", error.toString())
+                            progressDialog.dismiss()
+                        })
 
     }
 }
--- app/src/main/java/com/esteban/rentcar/services/Responses.kt
+++ app/src/main/java/com/esteban/rentcar/services/Responses.kt
@@ -5,3 +5,13 @@ object ListCarsResponse {
                    val type: String, val model: String, val rental: Rental)
     data class Rental(val id: Int, val name: String)
 }
+
+object DetailCarResponse {
+    data class Result(val id: Int, val brand: String,val thumbnail: String, val price: Int,
+                      val type: String, val model: String, val rental: Rental,
+                      val plate: String, val rating: Int, val capacity: Int,
+                      val transmission: String, val doors: Int, val color: String, val kms: Int,
+                      val picture: List<String>)
+
+    data class Rental(val id: Int, val name: String)
+}
