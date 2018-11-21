eedae20a5570a6f76146fec1a4207903f3186c82
estebanr14 <estebanrestrepor.14@gmail.com>
2018-11-21 15:09:38 -0500

    Style improve

--- app/src/main/java/com/esteban/rentcar/CarDetail.kt
+++ app/src/main/java/com/esteban/rentcar/CarDetail.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import com.esteban.rentcar.model.Car
 import com.esteban.rentcar.services.IRentyApi
 import com.squareup.picasso.Picasso
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -60,7 +62,14 @@ class CarDetail : AppCompatActivity() {
                                 kms.text = car.kms.toString()
 
                                 for(url in car.picture) {
-                                    Picasso.get().load(url).into(images)
+                                    var newView: ImageView //crea una instancia de imageview en tiempo de ejecucion
+                                    newView = ImageView(this)//La configura en el contexto, en este caso sería car_Details.kt
+                                    layout_scroll.addView(newView)//layout_scroll es el LInear layour que hay dentro del scroll, dentro van a ir todos los imagesviews. Añada la instancia de imageview creada
+                                    //ACÁ PONER EL TAMAÑO DE LA IMAGEN
+                                    newView.layoutParams.height = 900
+                                    newView.layoutParams.width = 900
+                                    //Acá se le setea el url al imageview con Picasso,
+                                    Picasso.get().load(url).into(newView)
                                 }
                             }
                             progressDialog.dismiss()
@@ -70,6 +79,29 @@ class CarDetail : AppCompatActivity() {
                             Log.e("errrrrrrr", error.toString())
                             progressDialog.dismiss()
                         })
+
+       /* var x =5 // en vez de una variable, es un array de url's
+        var list = ArrayList<String>()
+        list.add("https://i.imgur.com/IyEp7mf.jpg")
+        list.add("https://i.imgur.com/XEgyzCW.jpg")
+        list.add("https://i.imgur.com/72bQoTW.jpg")
+        list.add("https://i.imgur.com/fbPHUCn.jpg")
+        list.add("https://i.imgur.com/1wrP717.jpg")
+
+        //se recorre el array con un ciclo
+        while (x > 0) {
+            var newView: ImageView //crea una instancia de imageview en tiempo de ejecucion
+            newView = ImageView(this)//La configura en el contexto, en este caso sería car_Details.kt
+            layout_scroll.addView(newView)//layout_scroll es el LInear layour que hay dentro del scroll, dentro van a ir todos los imagesviews. Añada la instancia de imageview creada
+            //ACÁ PONER EL TAMAÑO DE LA IMAGEN
+            newView.layoutParams.height = 900
+            newView.layoutParams.width = 900
+            //Acá se le setea el url al imageview con Picasso,
+            Picasso.get().load(list[x-1]).into(newView)
+
+            x--
+        }*/
+
 
     }
 }
